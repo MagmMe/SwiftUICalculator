44f9c417f4e8fe334e2a01534689b9588448883f
MagmMe <marcin@magm.me>
2021-07-21 09:38:39 +0200

    Initial Commit

---
+++ SwiftUI Calculator APP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Calculator APP
+//
+//  Created by Marcin Magiera on 21/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Calculator APP/SwiftUI_Calculator_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Calculator_APPApp.swift
+//  SwiftUI Calculator APP
+//
+//  Created by Marcin Magiera on 21/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Calculator_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
