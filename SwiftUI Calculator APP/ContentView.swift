99dfffdea2b0138553b92e837cc6789d31400d7e
MagmMe <marcin@magm.me>
2021-07-21 16:57:26 +0200

    Created array of buttons and putting in in Hstachs and Vstacks

--- SwiftUI Calculator APP/ContentView.swift
+++ SwiftUI Calculator APP/ContentView.swift
@@ -8,15 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let  buttons = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
+    
     var body: some View {
         VStack{
-            HStack{
-                ForEach(["7", "8", "9", "X"], id: \.self){button in
-                    Text(button)
-                        .font(.system(size: 32))
-                        .frame(width:80, height:80)
+            
+            ForEach(buttons, id: \.self){row in
+                HStack{
+                    ForEach(row, id: \.self){button in
+                        Text(button)
+                            .font(.system(size: 32))
+                            .frame(width:80, height:80)
+                    }
                 }
             }
+            
+            
             
         }
     }
