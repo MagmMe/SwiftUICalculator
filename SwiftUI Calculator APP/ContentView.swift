4fc5664ae00cf958217a25c7d0c6e5c0391562af
MagmMe <marcin@magm.me>
2021-07-22 08:48:03 +0200

    Add function which calculate rounds buttons

--- SwiftUI Calculator APP/ContentView.swift
+++ SwiftUI Calculator APP/ContentView.swift
@@ -29,24 +29,26 @@ struct ContentView: View {
                 }.padding()
               
                 ForEach(buttons, id: \.self){row in
-                    HStack{
+                    HStack (spacing: 12){
                         ForEach(row, id: \.self){button in
                             Text(button)
                                 .font(.system(size: 32))
-                                .frame(width:80, height:80)
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
                                 .foregroundColor(.white)
                                 .background(Color.yellow)
-                                .cornerRadius(40)
-                                
+                                .cornerRadius(self.buttonWidth())
                         }
                     }
                 }
             }.padding(.bottom)
         }
-        
-       
+    }
+    
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
